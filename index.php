73e6ebc725c13881ed8b1fb7e9812bb1beb7db84
Loïc Herr <loic@herrloc.fr>
2016-11-24 16:26:21 +0100

    Page globale actualité

--- index.php
+++ index.php
@@ -1,19 +1,51 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+<!-- header -->
+<header class="header" role="banner"> 
 
-			<?php get_template_part('loop'); ?>
+    <?php include('logo-nav.php');?>
 
-			<?php get_template_part('pagination'); ?>
+    <div class="headerContent">
+        <div>     
+            <p class="h1">Actualités</p>
+        </div>
+    </div>
 
-		</section>
-		<!-- /section -->
-	</main>
+</header>
+<!-- /header -->
 
-<?php get_sidebar(); ?>
+<?php
+    // Les news
+    $argsPosts = array(
+        'post_type'		=> 'post',
+        'posts_per_page' => -1,
+    ); 
+
+    $queryPosts = new WP_Query( $argsPosts );
+?>
+<?php if( $queryPosts->have_posts() ): ?>
+<section class="wrapperPadding news">
+    <div class="wrapper-title">
+        <h2>Actualités du mariage</h2>
+        <p class="subtitle">Suivez l'actualité du mariage</p>
+        <svg viewBox="0 0 100 100" width="50" height="50"><use xlink:href="#icon-fleur"></use></svg>
+    </div>	
+     
+<?php while( $queryPosts->have_posts() ) : $queryPosts->the_post(); ?>
+   <div class="singleArticle">
+       <div class="contentSingle">
+           <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+           <p><?php the_excerpt();?></p>
+           <p><a href="<?php the_permalink();?>" class="bt btVide">Lire l'article</a></p>
+       </div>
+       <div class="imgSingle">
+           <a href="<?php the_permalink();?>"><?php the_post_thumbnail('s300');?></a>
+       </div>
+   </div>   
+<?php endwhile;?>
+    
+</section>
+<?php endif;?>
 
 <?php get_footer(); ?>
