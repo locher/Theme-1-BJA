f01bd012b164ef13a029ca6d2d53e188635ad994
Loïc Herr <loic@herrloc.fr>
2016-12-05 09:58:28 +0100

    date format bloc rappel rsvp

--- blocs/rappelRSVP.php
+++ blocs/rappelRSVP.php
@@ -11,9 +11,18 @@ if(strtotime($expire) > strtotime(now)):
         <h2>Vous êtes invité à notre mariage !</h2>
         <svg viewBox="0 0 100 100" width="50" height="50"><use xlink:href="#icon-fleur"></use></svg>
     </div>
-    <p>Veuillez s'il vous plait confirmer votre présence avant le <?php the_field('reponse_souhaitee_avant', 'option'); ?></p>
+    
+    <?php     
+        $dateformat = "j F";
+        $unixtimestamp = strtotime(get_field('reponse_souhaitee_avant', 'option', false));
+    ?>
+    <p>Veuillez s'il vous plait confirmer votre présence avant le <strong><?php echo date_i18n($dateformat, $unixtimestamp); ?></strong></p>
     <a href="<?php echo ods_getTemplatePermalink('template_formReponse.php'); ?>" class="bt">Votre réponse</a>
 </div>
 
 
 <?php endif;?>
+
+
+
+
