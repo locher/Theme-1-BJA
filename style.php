35e61259805e40f975f29660e87b739e63d91f0f
Loïc Herr <loic@herrloc.fr>
2016-12-06 15:26:59 +0100

    Choix de la couleur principale dans l'admin

---
+++ style.php
@@ -0,0 +1,12 @@
+<?php
+    $absolute_path = explode('wp-content', $_SERVER['SCRIPT_FILENAME']);
+    $wp_load = $absolute_path[0] . 'wp-load.php';
+    require_once($wp_load);
+
+    header('Content-type: text/css');
+    header('Cache-control: must-revalidate');
+
+    $colorAdmin = get_field('couleur_principale', 'option', false);
+
+    echo str_replace("#ff0000", $colorAdmin, file_get_contents('style.css'));
+?>
