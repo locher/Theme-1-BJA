8a9fb9b02e841432b1cf73b173fa88cde023f475
Loïc Herr <loic@herrloc.fr>
2016-12-23 16:23:19 +0100

    Typo

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 
 </div><!--.wrapper-->
                     
-                    
+                    <script src="https://use.typekit.net/icv7zgx.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
 						<?php wp_footer(); ?>
 	</body>
 </html>
