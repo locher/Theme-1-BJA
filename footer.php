e14a452e3132c8e7c1cbaa2728606c170eb4b56d
Loïc Herr <loic@herrloc.fr>
2017-02-07 10:12:45 +0100

    Footer bj amr

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
+<footer>
+    <a href="http://bonjouramour.fr" target="_blank" title="Bonjour Amour, votre site de mariage">Site de mariage conçu avec <svg viewBox="0 0 100 100" width="50" height="50"><use xlink:href="#icon-coeur"></use></svg>par <span>Bonjour Amour</span></a>
+</footer>
 
 </div><!--.wrapper-->
                     
